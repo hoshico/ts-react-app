e26f90b4a90bb1b5dcf58cac421171637b1e878e
hoshico <melancholinista.yasu@gmail.com>
2021-12-04 18:42:55 +0900

    add Todo.tsx

---
+++ src/Todo.tsx
@@ -0,0 +1,13 @@
+import { findAllByDisplayValue } from "@testing-library/dom";
+
+type TodoType = {
+    userId: number;
+    title: string;
+    completed?: boolean;
+}
+
+export const Todo = (props: TodoType) => {
+    const { title, userId, completed = false } = props;
+    const completeMark = completed ? "[完]" : "[未]"
+    return <p>{`${completeMark} ${title}(ユーザー:${userId})`}</p>
+}
